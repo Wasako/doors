7c06641449a22e546f0aae7a9091c1303c76c341
Adrian Jablonka <jablonka.adrian@gmail.com>
2016-07-05 16:24:23 +0200

    Gravity level Adrian

---
+++ Assets/Scripts/Controllers/Move2D.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move2D : MonoBehaviour {
+    public Vector3 right;
+    public float power = 15;
+    public float jumpPower = 40;
+    public float jumpTime = 2;
+    public float endJumpTime = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        GetComponent<Rigidbody>().AddForce(right*Input.GetAxis("Horizontal")*power);
+        if (Input.GetButtonDown("Jump") && jumpTime > endJumpTime)
+        {
+            GetComponent<Rigidbody>().AddForce(-Physics.gravity * jumpPower);
+        }
+
+        if(endJumpTime <= jumpTime)
+        {
+            endJumpTime += Time.deltaTime;
+        }
+
+        Debug.DrawRay(transform.position + transform.up, Physics.gravity, Color.red);
+        Debug.DrawRay(transform.position, right*10, Color.green);
+	}
+
+
+
+}
---
+++ Assets/Scripts/Gravity/GravityChanger.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityChanger : MonoBehaviour {
+    public GameObject floor;
+    public float gravityMultiply = 10;
+    public Vector3 newGravity;
+    public Vector3 rightSite;
+
+	// Use this for initialization
+	void Start () {
+        if (floor != null)
+        {
+            if (newGravity == Vector3.zero)
+            {
+                newGravity = floor.transform.forward * gravityMultiply;
+            }
+
+            if (rightSite == Vector3.zero)
+            {
+                rightSite = floor.transform.up;
+            }
+        }
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other) {
+        Physics.gravity = newGravity;
+        other.GetComponent<Move2D>().right = rightSite;
+    }
+
+}
