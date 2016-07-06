926864cf8c3ba810918a8896bb133db945c744e2
Kristian Mortensen <kristianmortensen@Kristians-MacBook-Air.local>
2016-07-06 18:27:41 +0200

    sdasd

---
+++ Assets/Scripts/DrawForward.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawForward : MonoBehaviour {
+
+
+	void OnDrawGizmosSelected(){
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine( this.transform.position, this.transform.position + (this.transform.right * 100f) );
+	}
+	/*void OnDrawGizmo(){
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine( this.transform.position, this.transform.position + (this.transform.right * 100f) );
+	}*/
+}
