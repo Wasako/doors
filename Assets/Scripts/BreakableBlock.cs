c1cba00d282b14a8db200e21e8065bfa6c405df8
Marek <kosmarek11@gazeta.pl>
2016-07-07 20:47:24 +0200

    breakable block

--- Assets/Scripts/BreakableBlock.cs
+++ Assets/Scripts/BreakableBlock.cs
@@ -4,17 +4,34 @@ using System.Collections;
 public class BreakableBlock : MonoBehaviour {
     public int Durability = 3;
 
-    Sprite sprite;
-
-    void Awake()
-    {
-        sprite = GetComponent<Sprite>();
-    }
+    public Sprite NewBlock, Damaged, AlmostDestroyed;
+   
    
     void OnCollisionEnter(Collision coll)
     {
         if(coll.gameObject.tag == "Player")
         {
+            switch(--Durability)
+            {
+                case 0:
+                    gameObject.SetActive(false);
+                    break;
+
+                case 1:
+                    GetComponent<SpriteRenderer>().sprite = AlmostDestroyed;
+                    break;
+                case 2:
+                    GetComponent<SpriteRenderer>().sprite = Damaged;
+                    break;
+
+                case 3:
+                    GetComponent<SpriteRenderer>().sprite = NewBlock;
+
+                    break;
+
+                default:
+                    break;
+            }
 
         }
     }
