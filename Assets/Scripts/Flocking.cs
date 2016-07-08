298940073fa591d8ddbae42a4a45e2f9d3b0353b
Jan Wasak <Jan Wasak>
2016-07-08 10:32:48 +0200

    updated flocking controller

--- Assets/Scripts/Flocking.cs
+++ Assets/Scripts/Flocking.cs
@@ -4,15 +4,16 @@ using System.Collections;
 public class Flocking : MonoBehaviour {
     //public Transform flyTarget;
     // Use this for initialization
+    public GameObject leader;
     Vector2 point = new Vector2(0, 0);
     int neighbourCount = 0;
     GameObject[] agentArray;
     public float flockingRadius = 100;
     Vector2 velocityVector;
     public float speed = 1;
-    public float alignmentWeight, cohesionWeight, separationWeight;
+    public float alignmentWeight, cohesionWeight, separationWeight, alignmentWeightLeader, cohesionWeightLeader, separationWeightLeader;
     void Start () {
-	
+        leader = GameObject.FindGameObjectWithTag("leader");
 	}
 	
 	// Update is called once per frame
@@ -20,12 +21,27 @@ public class Flocking : MonoBehaviour {
         Vector2 alignment = computeAlignment();
         Vector2 cohesion = computeCohesion();
         Vector2 separation = computeSeparation();
-        velocityVector = (alignment * alignmentWeight + cohesion * cohesionWeight + separation * separationWeight) * speed;
+        Vector2 alignmentLeader = computeAlignmentLeader();
+        Vector2 cohesionLeader = computeCohesionLeader();
+        Vector2 separationLeader = computeSeparationLeader();
+
+        velocityVector = 
+                (alignment * alignmentWeight + 
+                cohesion * cohesionWeight + 
+                separation * separationWeight +
+                alignmentLeader * alignmentWeightLeader +
+                cohesionLeader * cohesionWeightLeader +
+                separationLeader * separationWeightLeader) * speed;
+
+
         gameObject.GetComponent<Rigidbody2D>().velocity = velocityVector;
         //Debug.Log("final vector " + velocityVector.x + " " + velocityVector.y);
+        //transform.LookAt(leader.transform);
+        Quaternion rotation = Quaternion.LookRotation
+             (leader.transform.position - transform.position, transform.TransformDirection(Vector3.up));
+        transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
 
-        
-	}
+    }
     public Vector2 computeAlignment()
     {
         int neighbourCount = 0;
@@ -66,15 +82,16 @@ public class Flocking : MonoBehaviour {
                 cohesionVector.x += agent.transform.position.x;
                 cohesionVector.y += agent.transform.position.y;
                 neighbourCount++;
-               //Debug.Log("compute cohesion");
+              // Debug.Log("compute cohesion");
             }
 
         }
 
         cohesionVector /= neighbourCount;
         cohesionVector = new Vector2(cohesionVector.x - gameObject.transform.position.x, cohesionVector.y - gameObject.transform.position.y);
+        cohesionVector *= 1;
         cohesionVector.Normalize();
-        Debug.Log("cohesion vector " + cohesionVector.x + " " + cohesionVector.y);
+        //Debug.Log("cohesion vector " + cohesionVector.x + " " + cohesionVector.y);
         return cohesionVector;
     }
     public Vector2 computeSeparation()
@@ -98,6 +115,61 @@ public class Flocking : MonoBehaviour {
         }
 
         separationVector /= neighbourCount;
+        separationVector.Normalize();
+        separationVector *= -1;
+        //Debug.Log("separation vector " + separationVector.x + " " + separationVector.y);
+        return separationVector;
+    }
+
+    public Vector2 computeAlignmentLeader()
+    {
+       
+        Vector2 alignmentVector = new Vector2(0, 0);
+        agentArray = GameObject.FindGameObjectsWithTag("bird");
+        
+                alignmentVector += leader.GetComponent<Rigidbody2D>().velocity;
+                neighbourCount++;
+                // Debug.Log("compute alighment");
+          
+
+       
+        alignmentVector.Normalize();
+        // Debug.Log("alignment vector " + alignmentVector.x + " " + alignmentVector.y);
+        return alignmentVector;
+    }
+    public Vector2 computeCohesionLeader()
+    {
+        
+        Vector2 cohesionVector = new Vector2(0, 0);
+        
+                cohesionVector.x += leader.transform.position.x;
+                cohesionVector.y += leader.transform.position.y;
+                neighbourCount++;
+           
+
+      
+        cohesionVector = new Vector2(cohesionVector.x - gameObject.transform.position.x, cohesionVector.y - gameObject.transform.position.y);
+        cohesionVector *= -1;
+        cohesionVector.Normalize();
+       /// Debug.Log("cohesion vector " + cohesionVector.x + " " + cohesionVector.y);
+        return cohesionVector;
+    }
+    public Vector2 computeSeparationLeader()
+    {
+        int neighbourCount = 0;
+        Vector2 separationVector = new Vector2(0, 0);
+        agentArray = GameObject.FindGameObjectsWithTag("bird");
+     
+
+
+           
+                separationVector.x += leader.transform.position.x - transform.position.x;
+                separationVector.y += leader.transform.position.y - transform.position.y;
+                neighbourCount++;
+           
+
+
+        
         separationVector.Normalize();
         separationVector *= 1;
         //Debug.Log("separation vector " + separationVector.x + " " + separationVector.y);
---
+++ Assets/mouseController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouseController : MonoBehaviour {
+    Vector3 pz;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        pz = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pz.z = 0;
+        gameObject.transform.position = pz;
+    }
+}
