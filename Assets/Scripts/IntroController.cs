43c72cef34c1ac2ea53e01b1440245ff9387ace7
Jan Wasak <Jan Wasak>
2016-07-09 04:07:29 +0200

    intro is ready and steady

--- Assets/Scripts/IntroController.cs
+++ Assets/Scripts/IntroController.cs
@@ -2,7 +2,9 @@
 using System.Collections;
 
 public class IntroController : MonoBehaviour {
-
+    public Animator textMenu;
+    public Animator introMovie;
+    //public Animation introMovie;
 	// Use this for initialization
 	void Start () {
 	
@@ -12,4 +14,26 @@ public class IntroController : MonoBehaviour {
 	void Update () {
 	
 	}
+
+    public void introPlay()
+    {
+
+        textMenu.SetTrigger("playTrigger");
+    }
+    public void introContinue()
+    {
+
+        textMenu.SetTrigger("continueTrigger");
+        Debug.Log("Continue");
+    }
+    public void startMovie()
+    {
+        introMovie.enabled = true;
+    }
+    public void startGame ()
+    {
+        int i = Application.loadedLevel;
+        Application.LoadLevel(i + 1);
+    }
+
 }
